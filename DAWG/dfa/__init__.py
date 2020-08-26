eddbf8d6a06d69285c1a27d36cd4b9033c3d90d7
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-08-25 22:00:03 -0300

    Finish DFA

--- DAWG/dfa/__init__.py
+++ DAWG/dfa/__init__.py
@@ -1,15 +1,50 @@
-class DFAutomaton:
-    def __init__(
-        self, states, alphabet, transition_function, start_state, final_states
-    ):
+from abc import ABCMeta, abstractmethod
+
+
+class DFA(metaclass=ABCMeta):
+    def __init__(self, states, alphabet, start_state, final_states):
         self._states = states
         self._alphabet = alphabet
-        self._transition_function = transition_function
         self._start_state = start_state
         self._final_states = final_states
 
+    @abstractmethod
+    def _transition_function(self, state, symbol):
+        pass
+
+    def _extended_transition_function(self, state, string):
+        if not string:
+            return state
+        else:
+            x = string[0:-1]
+            a = string[-1]
+            return self._transition_function(
+                self._extended_transition_function(state, x), a
+            )
+
+    def accept(self, string):
+        return (
+            self._extended_transition_function(self._start_state, string)
+            in self._final_states
+        )
+
 
 if __name__ == "__main__":
-    f = None
-    s = None
-    a = DFAutomaton({}, {}, f, s, {})
+
+    class A(DFA):
+        def __init__(self):
+            super().__init__({0, 1, 2}, {"0", "1"}, 0, {2})
+            self._transition_table = {
+                (0, "0"): 1,
+                (0, "1"): 0,
+                (1, "0"): 1,
+                (1, "1"): 2,
+                (2, "0"): 2,
+                (2, "1"): 2,
+            }
+
+        def _transition_function(self, state, symbol):
+            return self._transition_table[(state, symbol)]
+
+    a = A()
+    print(a.accept("10"))
