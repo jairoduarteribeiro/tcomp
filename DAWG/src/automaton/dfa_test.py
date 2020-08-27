43e331a6529b8b1fd89768ffecafd362fbe5dbea
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-08-27 07:49:31 -0300

    Create test for DFA ext transition function

--- DAWG/src/automaton/dfa_test.py
+++ DAWG/src/automaton/dfa_test.py
@@ -27,6 +27,52 @@ class DFATestCase(unittest.TestCase):
         self.assertEqual(self.a._transition_function(2, '0'), 2)
         self.assertEqual(self.a._transition_function(2, '1'), 1)
 
+    def test_extended_transition_function(self):
+        self.assertEqual(self.a._extended_transition_function(0, ''), 0)
+        self.assertEqual(self.a._extended_transition_function(0, '0'), 2)
+        self.assertEqual(self.a._extended_transition_function(0, '1'), 0)
+        self.assertEqual(self.a._extended_transition_function(0, '01'), 1)
+        self.assertEqual(self.a._extended_transition_function(0, '11101'), 1)
+        self.assertEqual(
+            self.a._extended_transition_function(0, '11100001'), 1)
+        self.assertEqual(
+            self.a._extended_transition_function(0, '11100001000'), 1)
+        self.assertEqual(
+            self.a._extended_transition_function(0, '11100001111'), 1)
+        self.assertEqual(self.a._extended_transition_function(0, '111'), 0)
+        self.assertEqual(self.a._extended_transition_function(0, '1110'), 2)
+        self.assertEqual(self.a._extended_transition_function(0, '1110000'), 2)
+
+        self.assertEqual(self.a._extended_transition_function(1, ''), 1)
+        self.assertEqual(self.a._extended_transition_function(1, '0'), 1)
+        self.assertEqual(self.a._extended_transition_function(1, '1'), 1)
+        self.assertEqual(self.a._extended_transition_function(1, '01'), 1)
+        self.assertEqual(self.a._extended_transition_function(1, '11101'), 1)
+        self.assertEqual(
+            self.a._extended_transition_function(1, '11100001'), 1)
+        self.assertEqual(
+            self.a._extended_transition_function(1, '11100001000'), 1)
+        self.assertEqual(
+            self.a._extended_transition_function(1, '11100001111'), 1)
+        self.assertEqual(self.a._extended_transition_function(1, '111'), 1)
+        self.assertEqual(self.a._extended_transition_function(1, '1110'), 1)
+        self.assertEqual(self.a._extended_transition_function(1, '1110000'), 1)
+
+        self.assertEqual(self.a._extended_transition_function(2, ''), 2)
+        self.assertEqual(self.a._extended_transition_function(2, '0'), 2)
+        self.assertEqual(self.a._extended_transition_function(2, '1'), 1)
+        self.assertEqual(self.a._extended_transition_function(2, '01'), 1)
+        self.assertEqual(self.a._extended_transition_function(2, '11101'), 1)
+        self.assertEqual(
+            self.a._extended_transition_function(2, '11100001'), 1)
+        self.assertEqual(
+            self.a._extended_transition_function(2, '11100001000'), 1)
+        self.assertEqual(
+            self.a._extended_transition_function(2, '11100001111'), 1)
+        self.assertEqual(self.a._extended_transition_function(2, '111'), 1)
+        self.assertEqual(self.a._extended_transition_function(2, '1110'), 1)
+        self.assertEqual(self.a._extended_transition_function(2, '1110000'), 1)
+
     def test_accept(self):
         self.assertTrue(self.a.accept('01'))
         self.assertTrue(self.a.accept('11101'))
