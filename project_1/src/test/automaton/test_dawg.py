56cdda451714b3a9903637cf68640cefa327482c
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-08-28 23:39:44 -0300

    Use class name instead of variable

--- project_1/src/test/automaton/test_dawg.py
+++ project_1/src/test/automaton/test_dawg.py
@@ -8,26 +8,26 @@ class DAWGTestCase(unittest.TestCase):
         cls.a = DAWG()
 
     def test_build_sample(self):
-        self.assertEqual(self.a._build_sample({'aba +', 'baa +', 'b +', 'a',
+        self.assertEqual(DAWG._build_sample({'aba +', 'baa +', 'b +', 'a',
                                                'bab', 'aaa'}),
                          {'+': frozenset({'aba', 'baa', 'b'}), '-': frozenset({
                              'a', 'bab', 'aaa'
                          })})
 
     def test_p(self):
-        self.assertEqual(self.a._p('abc'), frozenset({'', 'a', 'ab', 'abc'}))
-        self.assertEqual(self.a._p(frozenset({'aba', 'baa', 'b'})), frozenset({'', 'a', 'ab',
+        self.assertEqual(DAWG._p('abc'), frozenset({'', 'a', 'ab', 'abc'}))
+        self.assertEqual(DAWG._p(frozenset({'aba', 'baa', 'b'})), frozenset({'', 'a', 'ab',
                                                                                'aba', 'b', 'ba',
                                                                                'baa'}))
 
     def test_left_quotients(self):
-        self.assertEqual(self.a._left_quotients('', frozenset({'aba', 'baa', 'b'})),
+        self.assertEqual(DAWG._left_quotients('', frozenset({'aba', 'baa', 'b'})),
                          frozenset({'aba', 'baa', 'b'}))
-        self.assertEqual(self.a._left_quotients('a', frozenset({'aba', 'baa', 'b'})), frozenset({
+        self.assertEqual(DAWG._left_quotients('a', frozenset({'aba', 'baa', 'b'})), frozenset({
             'ba'}))
 
     def test_v(self):
-        self.assertEqual(self.a._v(frozenset({'aba', 'baa', 'b'})),
+        self.assertEqual(DAWG._v(frozenset({'aba', 'baa', 'b'})),
                          {'': frozenset({'b', 'baa', 'aba'}),
                           'a': frozenset({'ba'}),
                           'ab': frozenset({'a'}),
@@ -37,7 +37,7 @@ class DAWGTestCase(unittest.TestCase):
                           'baa': frozenset({''})})
 
     def test_l(self):
-        self.assertEqual(self.a._l({'': frozenset({'b', 'baa', 'aba'}),
+        self.assertEqual(DAWG._l({'': frozenset({'b', 'baa', 'aba'}),
                                     'a': frozenset({'ba'}),
                                     'ab': frozenset({'a'}),
                                     'aba': frozenset({''}),
@@ -54,16 +54,16 @@ class DAWGTestCase(unittest.TestCase):
                               frozenset({'a'})})
 
     def test_potency(self):
-        self.assertEqual(self.a._potency({(1, 2), (1, 3), (1, 5), (2, 4), (3, 4),
+        self.assertEqual(DAWG._potency({(1, 2), (1, 3), (1, 5), (2, 4), (3, 4),
                                           (4, 5)}, 1, 5),
                          {(1, 2): 1, (1, 3): 1, (1, 5): 1, (2, 4): 1, (3, 4): 1,
                           (4, 5): 1})
-        self.assertEqual(self.a._potency({(1, 2), (2, 3), (2, 4), (3, 4)}, 1, 4),
+        self.assertEqual(DAWG._potency({(1, 2), (2, 3), (2, 4), (3, 4)}, 1, 4),
                          {(1, 2): 2,
                           (2, 3): 1, (2, 4): 1, (3, 4): 1})
 
     def test_transition(self):
-        self.assertEqual(self.a._transition(3, 'aa',
+        self.assertEqual(DAWG._transition(3, 'aa',
                                             {(1, 2):
                                                  frozenset({'a'}),
                                              (1, 5):
@@ -77,7 +77,7 @@ class DAWGTestCase(unittest.TestCase):
                                                  frozenset({'a'}),
                                              (4, 5):
                                                  frozenset({'a'})}), frozenset({5}))
-        self.assertEqual(self.a._transition(1, 'b', {(1, 2):
+        self.assertEqual(DAWG._transition(1, 'b', {(1, 2):
                                                  frozenset({'a'}),
                                              (1, 5):
                                                  frozenset({'b'}),
