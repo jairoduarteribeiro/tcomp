c4d3d2d6813f2a2521a6a7941fb9abfc68c59ae5
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-08-29 20:39:49 -0300

    Calculate execution time

--- project_1/main.py
+++ project_1/main.py
@@ -1,4 +1,5 @@
 from os.path import join, dirname, realpath
+from time import time
 from src.automaton.dawg import DAWG
 from src.utils.file_utils import FileUtils
 
@@ -6,4 +7,6 @@ from src.utils.file_utils import FileUtils
 if __name__ == '__main__':
     path = join(dirname(realpath(__file__)), 'waltz.txt')
     data = FileUtils.prepare_data(path)
+    start_time = time()
     a = DAWG(data)
+    print(f'{time() - start_time} seconds.')
