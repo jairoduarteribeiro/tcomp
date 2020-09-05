d85d0cfb71dee7f7a264c158ebbabc0f5521dc99
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-09-05 10:35:40 -0300

    Calculate time to build DAWG and convert to DFA

--- project_1/main.py
+++ project_1/main.py
@@ -7,6 +7,13 @@ from src.utils.file_utils import FileUtils
 if __name__ == '__main__':
     path = join(dirname(realpath(__file__)), 'waltz.txt')
     data = FileUtils.prepare_data(path)
-    start_time = time()
-    a = DAWG(data)
-    print(f'{time() - start_time} seconds.')
+
+    # Build DAWG from dataset.
+    current_time = time()
+    dawg = DAWG(data)
+    print(f'Build DAWG (NFA): {time() - current_time} seconds.')
+
+    # Convert DAWG to DFA.
+    current_time = time()
+    dfa = dawg.convert_to_dfa()
+    print(f'Convert DAWG to DFA: {time() - current_time} seconds.')
