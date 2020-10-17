5247cb0ec8c99541f702c97ed6d7e5dd00cc5ffd
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-10-17 07:38:40 -0300

    Add list examples

--- project_2/src/grammar/cfg.py
+++ project_2/src/grammar/cfg.py
@@ -1,6 +1,22 @@
 class CFG:
-    def __init__(self, variables, terminals, productions, start_symbol):
+    def __init__(self, variables, terminals, productions, start_symbol, is_normalized=False):
         self._variables = frozenset(variables)
         self._terminals = frozenset(terminals)
         self._productions = frozenset(productions)
         self._start_symbol = start_symbol
+        self._is_normalized = is_normalized
+
+    def normalize(self):
+        if self._is_normalized:
+            return CFG(
+                variables=self._variables,
+                terminals=self._terminals,
+                productions=self._productions,
+                start_symbol=self._start_symbol,
+                is_normalized=self._is_normalized
+            )
+        else:
+            pass
+
+    def accept(self, string):
+        return False
--- project_2/src/main.py
+++ project_2/src/main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+from grammar.cfg import CFG
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    g1 = CFG(
+        variables={'F', 'O', 'A', 'N'},
+        terminals={'->', '/\\', '\\/', '!', '(', ')', 'p', 'q', 'r'},
+        productions={
+            ('F', ('O', '->', 'F')),
+            ('F', ('O',)),
+            ('O', ('O', '\\/', 'A')),
+            ('O', ('A',)),
+            ('A', ('A', '/\\', 'N')),
+            ('A', ('N',)),
+            ('N', ('!', 'N')),
+            ('N', ('(', 'F', ')')),
+            ('N', ('p',)),
+            ('N', ('q',)),
+            ('N', ('r',))
+        },
+        start_symbol='F'
+    )
+    print(g1.accept(string=('J', 'a', 'i', 'r', 'o')))
+
+    g2 = CFG(
+        variables={'D', 'H', 'I', 'L', 'M', 'O', 'T'},
+        terminals={
+            '<html>', '</html>', '<head>', '</head>', '<title>', '</title>', '<body>',
+            '</body>', '<b>', '</b>', '<ul>', '</ul>', '<ol>', '</ol>', '<li>', '</li>',
+            'theory of computation', 'automata', ' ', 'grammars'},
+        productions={
+            ('D', ('<html>', '<head>', '<title>', 'T', '</title>', '</head>', '<body>', 'H',
+                   '</body>', '</html>')),
+            ('H', ('I', 'H')),
+            ('H', ('I',)),
+            ('I', ('<b>', 'H', '</b>')),
+            ('I', ('T',)),
+            ('I', ('L',)),
+            ('L', ('<ul>', 'M', '</ul>')),
+            ('L', ('<ol>', 'M', '</ol>')),
+            ('M', ('M', 'O')),
+            ('M', ('O',)),
+            ('O', ('<li>', 'H', '</li>')),
+            ('T', ('theory of computation',)),
+            ('T', (' ',)),
+            ('T', ('automata',)),
+            ('T', ('grammars',))
+        },
+        start_symbol='D'
+    )
+    print(g2.accept(string=('J', 'a', 'i', 'r', 'o')))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
