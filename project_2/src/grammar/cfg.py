6bc9cdc49f71b36cdf6dbfbe56e73f1ffe2f62cb
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-10-17 14:43:33 -0300

    Add a method to remove epsilon transitions

--- project_2/src/grammar/cfg.py
+++ project_2/src/grammar/cfg.py
@@ -1,3 +1,6 @@
+from itertools import chain, combinations
+
+
 class CFG:
     def __init__(self, variables, terminals, productions, start_symbol, is_normalized=False):
         self._variables = frozenset(variables)
@@ -20,7 +23,7 @@ class CFG:
         variables = self._variables
         productions = self._productions
 
-        if len(tuple(filter(lambda p: start_symbol in p[-1], productions))) > 0:
+        if len(tuple(filter(lambda p: start_symbol in p[1], productions))) > 0:
             start_symbol = '$0'
             variables = variables.union((start_symbol,))
             productions = self._productions.union(((start_symbol, (self._start_symbol,)),))
@@ -30,6 +33,48 @@ class CFG:
             terminals=self._terminals,
             productions=productions,
             start_symbol=start_symbol
+        )
+
+    def _remove_epsilon(self):
+        def tuple_subset(tuple1, tuple2):
+            for t in tuple1:
+                if t not in tuple2:
+                    return False
+            return True
+
+        productions = self._productions
+        removed_productions = frozenset()
+
+        while True:
+            curr = \
+                next((p for p in productions if '' in p[1] and p[0] != self._start_symbol), None)
+
+            if not curr:
+                break
+
+            removed_productions = removed_productions.union((curr,))
+            productions = productions.difference((curr,))
+            new_productions = frozenset()
+            curr_right = frozenset(filter(lambda p: curr[0] in p[1], productions))
+
+            for p in curr_right:
+                diff = tuple(filter(lambda e: e != curr[0], p[1]))
+                candidates = tuple(
+                    chain.from_iterable(combinations(p[1], r) for r in range(len(p[1]) + 1)))
+                candidates = tuple(filter(lambda c: tuple_subset(diff, c), candidates))
+
+                for c in candidates:
+                    new_productions = \
+                        new_productions.union(((p[0], c if c != () else ('',)),))
+
+            new_productions = new_productions.difference(removed_productions)
+            productions = productions.union(new_productions)
+
+        return CFG(
+            variables=self._variables,
+            terminals=self._terminals,
+            productions=productions,
+            start_symbol=self._start_symbol
         )
 
     def normalize(self):
@@ -42,8 +87,16 @@ class CFG:
                 is_normalized=self._is_normalized
             )
         else:
-            # Check if start symbol is on the right side of productions.
-            return self._replace_start_symbol()
+            g = self\
+                ._replace_start_symbol()\
+                ._remove_epsilon()
+            return CFG(
+                variables=g._variables,
+                terminals=g._terminals,
+                productions=g._productions,
+                start_symbol=g._start_symbol,
+                is_normalized=True
+            )
 
     def accept(self, string):
         if self._is_normalized:
--- project_2/test/grammar/test_cfg.py
+++ project_2/test/grammar/test_cfg.py
@@ -41,3 +41,66 @@ class CFGTestCase(TestCase):
             start_symbol='S'
         )
         self.assertEqual(g2._replace_start_symbol(), g2)
+
+    def test_remove_epsilon(self):
+        g1 = CFG(
+            variables={'$0', 'S', 'A', 'B'},
+            terminals={'a', 'b'},
+            productions={
+                ('$0', ('S',)),
+                ('S', ('A', 'S', 'A')),
+                ('S', ('a', 'B')),
+                ('A', ('B',)),
+                ('A', ('S',)),
+                ('B', ('b',)),
+                ('B', ('',))
+            },
+            start_symbol='$0'
+        )
+        self.assertEqual(g1._remove_epsilon(), CFG(
+            variables={'$0', 'S', 'A', 'B'},
+            terminals={'a', 'b'},
+            productions={
+                ('$0', ('S',)),
+                ('S', ('A', 'S', 'A')),
+                ('S', ('S', 'A')),
+                ('S', ('A', 'S')),
+                ('S', ('S',)),
+                ('S', ('a', 'B')),
+                ('S', ('a',)),
+                ('A', ('B',)),
+                ('A', ('S',)),
+                ('B', ('b',))
+            },
+            start_symbol='$0'
+        ))
+
+        g2 = CFG(
+            variables={'S', 'A', 'B'},
+            terminals={'a', 'b'},
+            productions={
+                ('S', ('A', 'B')),
+                ('A', ('a', 'A', 'A',)),
+                ('A', ('',)),
+                ('B', ('b', 'B', 'B')),
+                ('B', ('',))
+            },
+            start_symbol='S'
+        )
+        self.assertEqual(g2._remove_epsilon(), CFG(
+            variables={'S', 'A', 'B'},
+            terminals={'a', 'b'},
+            productions={
+                ('S', ('A', 'B')),
+                ('S', ('A',)),
+                ('S', ('B',)),
+                ('S', ('',)),
+                ('A', ('a', 'A', 'A')),
+                ('A', ('a', 'A')),
+                ('A', ('a',)),
+                ('B', ('b', 'B', 'B')),
+                ('B', ('b', 'B',)),
+                ('B', ('b',))
+            },
+            start_symbol='S'
+        ))
