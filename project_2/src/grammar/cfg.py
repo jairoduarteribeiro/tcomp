6c65f98193c54e1adb2e7ed4f10fa6ab475889ef
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-10-17 10:10:12 -0300

    Create a method for replace start symbol

--- project_2/src/grammar/cfg.py
+++ project_2/src/grammar/cfg.py
@@ -6,6 +6,32 @@ class CFG:
         self._start_symbol = start_symbol
         self._is_normalized = is_normalized
 
+    def __eq__(self, other):
+        result = True
+        result = result and self._variables == other._variables
+        result = result and self._terminals == other._terminals
+        result = result and self._productions == other._productions
+        result = result and self._start_symbol == other._start_symbol
+        return result
+
+    def _replace_start_symbol(self):
+        # Check if start symbol is on the right side of productions.
+        start_symbol = self._start_symbol
+        variables = self._variables
+        productions = self._productions
+
+        if len(tuple(filter(lambda p: start_symbol in p[-1], productions))) > 0:
+            start_symbol = '$0'
+            variables = variables.union((start_symbol,))
+            productions = self._productions.union(((start_symbol, (self._start_symbol,)),))
+
+        return CFG(
+            variables=variables,
+            terminals=self._terminals,
+            productions=productions,
+            start_symbol=start_symbol
+        )
+
     def normalize(self):
         if self._is_normalized:
             return CFG(
@@ -16,7 +42,11 @@ class CFG:
                 is_normalized=self._is_normalized
             )
         else:
-            pass
+            # Check if start symbol is on the right side of productions.
+            return self._replace_start_symbol()
 
     def accept(self, string):
-        return False
+        if self._is_normalized:
+            return False
+        else:
+            return self.normalize().accept(string)
--- project_2/test/grammar/test_cfg.py
+++ project_2/test/grammar/test_cfg.py
@@ -1,6 +1,43 @@
 from unittest import TestCase
+from grammar.cfg import CFG
 
 
 class CFGTestCase(TestCase):
-    def test_example(self):
-        self.assertTrue(True)
+    def test_replace_start_symbol(self):
+        g1 = CFG(
+            variables={'S', 'A', 'B'},
+            terminals={'a', 'b'},
+            productions={
+                ('S', ('A', 'S', 'A')),
+                ('S', ('a', 'B')),
+                ('A', ('B', 'b')),
+                ('B', ('b',)),
+                ('B', ('',))
+            },
+            start_symbol='S'
+        )
+        self.assertEqual(g1._replace_start_symbol(), CFG(
+            variables={'$0', 'S', 'A', 'B'},
+            terminals={'a', 'b'},
+            productions={
+                ('$0', ('S',)),
+                ('S', ('A', 'S', 'A')),
+                ('S', ('a', 'B')),
+                ('A', ('B', 'b')),
+                ('B', ('b',)),
+                ('B', ('',))
+            },
+            start_symbol='$0'
+        ))
+
+        g2 = CFG(
+            variables={'S', 'A'},
+            terminals={'a', 'b'},
+            productions={
+                ('S', ('A', 'B')),
+                ('S', ('a',)),
+                ('A', ('b',))
+            },
+            start_symbol='S'
+        )
+        self.assertEqual(g2._replace_start_symbol(), g2)
