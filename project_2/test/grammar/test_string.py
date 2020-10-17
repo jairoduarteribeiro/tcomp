0a15631f1dbefe2b66bdc5df15aa30d102aeef16
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-10-17 10:09:05 -0300

    Remove String class

--- project_2/test/grammar/test_string.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from src.grammar.string import String
-
-
-class StringTestCase(TestCase):
-    def test_equals(self):
-        s1 = String(['a'])
-        s2 = String(['a'])
-        self.assertTrue(s1 == s2)
