38bc3356982a14e41796dd5644e2018b382025ff
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-10-18 15:16:10 -0300

    Add methods to keep rules with 2 symbols

--- project_2/src/grammar/cfg.py
+++ project_2/src/grammar/cfg.py
@@ -9,14 +9,6 @@ class CFG:
         self._start_symbol = start_symbol
         self._is_normalized = is_normalized
 
-    def __eq__(self, other):
-        result = True
-        result = result and self._variables == other._variables
-        result = result and self._terminals == other._terminals
-        result = result and self._productions == other._productions
-        result = result and self._start_symbol == other._start_symbol
-        return result
-
     def _replace_start_symbol(self):
         # Check if start symbol is on the right side of productions.
         start_symbol = self._start_symbol
@@ -108,6 +100,87 @@ class CFG:
             start_symbol=self._start_symbol
         )
 
+    def _keep_rules_with_two_symbols(self):
+        variables = self._variables
+        productions = self._productions
+        new_productions = frozenset()
+        index = 0
+
+        while True:
+            curr = next((p for p in productions if len(p[1]) > 2), None)
+
+            if not curr:
+                break
+
+            productions = productions.difference((curr,))
+            slc = curr[1][1:]
+            production = next((p for p in new_productions if slc == p[1]), None)
+
+            if production:
+                productions = \
+                    productions.union(((curr[0], (curr[1][0], production[0])),))
+            else:
+                new_production = (f'#{index}', slc)
+                variables = variables.union((new_production[0],))
+                productions = \
+                    productions.union(((curr[0], (curr[1][0], new_production[0])),))
+                productions = productions.union((new_production,))
+                new_productions = new_productions.union((new_production,))
+                index = index + 1
+
+        return CFG(
+            variables=variables,
+            terminals=self._terminals,
+            productions=productions,
+            start_symbol=self._start_symbol
+        )
+
+    def _remove_terminals_from_no_unit_rules(self):
+        variables = self._variables
+        productions = self._productions
+        new_productions = frozenset()
+        index = 0
+
+        while True:
+            curr = \
+                next((p for p in productions if len(p[1] == 2 and (
+                        p[1][0] in self._terminals or p[1][1] in self._terminals
+                ))), None)
+
+            if not curr:
+                break
+
+            productions = productions.difference((curr,))
+            slc = curr[1][:1] if curr[1][0] in self._terminals else curr[1][1:]
+            production = next((p for p in new_productions if slc == p[1]), None)
+
+            if production:
+                if curr[1][1] in self._terminals:
+                    productions = \
+                        productions.union(((curr[0], (curr[1][0], production[0])),))
+                else:
+                    productions = \
+                        productions.union(((curr[0], (production[0], curr[1][1])),))
+            else:
+                new_production = (f'@{index}', slc)
+                variables = variables.union((new_production[0],))
+                if curr[1][1] in self._terminals:
+                    productions = \
+                        productions.union(((curr[0], (curr[1][0], new_production[0])),))
+                else:
+                    productions = \
+                        productions.union(((curr[0], (new_production[0], curr[1][1])),))
+                productions = productions.union((new_production,))
+                new_productions = new_productions.union((new_production,))
+                index = index + 1
+
+        return CFG(
+            variables=variables,
+            terminals=self._terminals,
+            productions=productions,
+            start_symbol=self._start_symbol
+        )
+
     def normalize(self):
         if self._is_normalized:
             return CFG(
@@ -118,10 +191,12 @@ class CFG:
                 is_normalized=self._is_normalized
             )
         else:
-            g = self\
-                ._replace_start_symbol()\
-                ._remove_epsilon()\
-                ._remove_unit_rules()
+            g = self \
+                ._replace_start_symbol() \
+                ._remove_epsilon() \
+                ._remove_unit_rules() \
+                ._keep_rules_with_two_symbols() \
+                ._remove_terminals_from_no_unit_rules()
             return CFG(
                 variables=g._variables,
                 terminals=g._terminals,
